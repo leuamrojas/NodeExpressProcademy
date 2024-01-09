da38802d1ed84940fd4a14bbadc7375ad08db1fe
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-09 12:07:22 -0600

    #87 Query and Aggregation Middleware

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -54,7 +54,7 @@ const movieSchema = new mongoose.Schema({
         type: Number,
         required: [true, 'Price is required field!']
     },
-    createdBy: string
+    createdBy: String
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true } //Make it accesible to use from code
@@ -70,16 +70,57 @@ movieSchema.virtual('durationInHours').get(function() {
 //insertMany, findByIdAndUpdate will not work
 //You can have multiple middlewares and they will be executed in the order of appearance
 movieSchema.pre('save', function(next) {
+    //'this' points to the current document
     this.createdBy = 'LOGGED_IN_USER'; //We add the logged in username before the doc is saved (pre-hook on save event)
 
     next();
 });
 
+//Document Middleware: executed after the document is saved in DB
 movieSchema.post('save', function(doc, next) {
     const content = `A new movie document with name ${doc.name} has been created by ${doc.createdBy}\n`;
     fs.writeFileSync('./Log/log.txt', content, {flag: 'a'}, (err) => {
         console.log(err.message);
     });
+
+    next();
+});
+
+//Query Middleware
+//It won't work for findById because it uses findOne behind scenes (see workaround below)
+// movieSchema.pre('find', function(next) {
+//     //'this' points to the query object (returned by Movie.find())
+//     this.find({ releaseDate: { $lte: Date.now() } }) // return only movies from/before current date
+
+//     next();
+// });
+
+// will execute for any query method starting with 'find'
+movieSchema.pre(/^find/, function(next) {
+    //'this' points to the query object (returned by Movie.find())
+    this.find({ releaseDate: { $lte: Date.now() } }) // return only movies until current date
+    this.startTime = Date.now();
+
+    next();
+});
+
+movieSchema.post(/^find/, function(docs, next) {
+    //'this' points to the query object (returned by Movie.find())
+    this.find({ releaseDate: { $lte: Date.now() } }) // return only movies until current date
+    this.endTime = Date.now();
+    const content = `Query took ${this.endTime - this.startTime} milliseconds to fetch the documents\n`;
+    fs.writeFileSync('./Log/log.txt', content, {flag: 'a'}, (err) => {
+        console.log(err.message);
+    });
+
+    next();
+});
+
+//Aggregate Middleware
+movieSchema.pre('aggregate', function(next) {
+    // add an aggregation stage to match only those documents until current date 
+    // before performing aggregation (getMovieStats and getMovieByGenre)
+    this.pipeline().unshift({ $match: { releaseDate: { $lte: new Date() } } })
 
     next();
 });
