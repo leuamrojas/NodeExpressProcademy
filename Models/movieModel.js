b2f04048de087c9411a0690459d974589e56b41e
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-09 12:34:41 -0600

    #88 Data Validators: Built-in validators

--- Models/movieModel.js
+++ Models/movieModel.js
@@ -6,6 +6,8 @@ const movieSchema = new mongoose.Schema({
         type: String,
         required: [true, 'Name is required field!'],
         unique: true,
+        maxlength: [100, 'Movie name must not have more than 100 characters'],
+        minlength: [4, 'Movie name must have at least 4 characters'],
         trim: true
     },
     description: { 
@@ -18,7 +20,9 @@ const movieSchema = new mongoose.Schema({
         required: [true, 'Duration is required field!']
     },
     ratings: {
-        type: Number
+        type: Number,
+        min: [1, 'Ratings must be 1.0 or above'],
+        max: [10, 'Ratings must be 10.0 or below']
     },
     totalRating: {
         type: Number
@@ -36,7 +40,11 @@ const movieSchema = new mongoose.Schema({
     },
     genres: {
         type: [String],
-        required: [true, 'Genres is required field!']
+        required: [true, 'Genres is required field!'],
+        enum: { // can only be used on string types
+            values: ['Action', 'Adventure', 'Sci-Fi', 'Thriller', 'Crime', 'Drama', 'Comedy', 'Romance', 'Biography'],
+            message: "The genre does not exist"
+        }
     },
     directors: {
         type: [String],
