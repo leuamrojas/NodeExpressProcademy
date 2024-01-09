439006972b6b86decc6fb05e13635ebd4a80dfd2
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-09 13:31:38 -0600

    #91 Global Error Handling Middleware

--- app.js
+++ app.js
@@ -21,10 +21,28 @@ app.use('/api/v1/users', moviesRouter);
 //will execute for all the urls for which we have not defined a route
 //must always come last in the defined routes (if placed first, '/api/v1/movies' route would match the '*' pattern)
 app.all('*', (req, res, next) => {
-    res.status(404).json({
-        status: 'fail',
-        message: `Can't find ${req.originalUrl} on the server!`
-    })
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `Can't find ${req.originalUrl} on the server!`
+    // });
+    const err = new Error(`Can't find ${req.originalUrl} on the server!`);
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    // whenever we pass any object to 'next()', express assumes there was an error
+    // and will skip all other middleware functions in the stack 
+    // and directly call the global error handling middleware function
+    next(err); 
+});
+
+//Global Error Handling Middleware
+app.use((error, req, res, next) => {
+    error.statusCode = error.statusCode || 500;
+    error.status = error.status || 'error';
+    res.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message
+    });
 });
 
 
