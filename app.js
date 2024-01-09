056f457e518389cf1cec7c0750c0e3526371416f
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-09 13:16:51 -0600

    #90 Creating a default route

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const moviesRouter = require('./Routes/moviesRoutes')
 let app = express();
 
 app.use(express.json()); // Adds a middleware to add the request body to the request object
-app.use(express.static('./public'));
+app.use(express.static('./public')); // Serve static files
 // app.get('/api/v1/movies', getAllMovies);
 // app.get('/api/v1/movies/:id', getMovie);
 // app.post('/api/v1/movies', createMovie);
@@ -16,5 +16,16 @@ app.use(express.static('./public'));
 //Mount the router moviesRouter to path /api/v1/movies. The router is basically a middleware 
 //that will only be applied to those requests that contain the path in the url
 app.use('/api/v1/movies', moviesRouter);
+app.use('/api/v1/users', moviesRouter);
+
+//will execute for all the urls for which we have not defined a route
+//must always come last in the defined routes (if placed first, '/api/v1/movies' route would match the '*' pattern)
+app.all('*', (req, res, next) => {
+    res.status(404).json({
+        status: 'fail',
+        message: `Can't find ${req.originalUrl} on the server!`
+    })
+});
+
 
 module.exports = app;
