1b5719cb7e15bad6ffdf5d255dddbbed5de43c4d
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-10 11:56:35 -0600

    #95 Development and Production Error

--- Controllers/errorController.js
+++ Controllers/errorController.js
@@ -1,8 +1,34 @@
+
+const devErrors = (res, error) => {
+    res.status(error.statusCode).json({
+        status: error.statusCode,
+        message: error.message,
+        stackTrace: error.stack,
+        error: error
+    });
+}
+
+const prodErrors = (res, error) => {
+    if (error.isOperational) {
+        res.status(error.statusCode).json({
+            status: error.statusCode,
+            message: error.message,
+        });
+    } else { // Errors created by Mongoose (e.g. validation errors) are not operational
+        res.status(500).json({
+            status: 'error',
+            message: 'Something went wrong! Please, try again later.'
+        });
+    }    
+}
+
 module.exports = (error, req, res, next) => {
     error.statusCode = error.statusCode || 500;
     error.status = error.status || 'error';
-    res.status(error.statusCode).json({
-        status: error.statusCode,
-        message: error.message
-    });
+
+    if (process.env.NODE_ENV === 'development') {
+        devErrors(res, error);
+    } else if (process.env.NODE_ENV === 'production') {
+        prodErrors(res, error);
+    }    
 };
