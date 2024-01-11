5fe99839581ddb834d14df18677a2d11eff9d103
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-11 12:06:19 -0600

    #104 Encrypting Password

--- Models/userModel.js
+++ Models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 //name, email
 const userSchema = new mongoose.Schema({
@@ -22,8 +23,24 @@ const userSchema = new mongoose.Schema({
     },
     confirmPassword: {
         type: String,
-        required: [true, 'Please confirm your password']        
+        required: [true, 'Please confirm your password'],
+        validate: {
+            validator: function(val) {
+                return val === this.password;
+            },
+            message: 'Password and ConfirmPassword do not match'
+        }
     },
+});
+
+userSchema.pre('save', async function(next) {
+    if(!this.isModified('password')) return next();
+
+    // encrypt the password before saving it
+    // 12=salt length (the higher the value, the longer it takes to compute the hash)
+    this.password = await bcrypt.hash(this.password, 12); 
+    this.confirmPassword = undefined;
+    next();
 });
 
 const User = mongoose.model('User', userSchema);
