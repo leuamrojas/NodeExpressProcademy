2bf8440961f1daf3109bd232dac00409b9da3e93
Manuel Rojas <leuamrojas@hotmail.com>
2024-01-17 13:25:59 -0600

    #118 Using Cookie for Sending JWT

--- Utils/authResponse.js
+++ Utils/authResponse.js
@@ -7,8 +7,20 @@ const signToken = id => {
 }
 
 const createSendResponse = (user, statusCode, res) => {
-
     const token = signToken(user._id);
+
+    const options = {
+        maxAge: process.env.LOGIN_EXPIRES,
+        httpOnly: true //makes sure the cookie cannot be accessed or modified by the browser;
+    };
+
+    if (process.env.NODE_ENV === 'production') {
+        options.secure = true //makes sure the cookie is only sent on https
+    }
+
+    res.cookie('jwt', token, options);
+
+    user.password = undefined;
 
     res.status(statusCode).json({
         status: 'success',
